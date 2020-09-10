504ef03a1e436332a326cadb55abe1e3f1169a09
Sara A <calisaraalhindi@gmail.com>
2020-09-10 05:49:49 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,13 +9,10 @@ class GetRequester
   end
   
   def get_response_body
-    uri = URI.parse(url)
-response = Net::HTTP.get_response_body(uri)
-response.body
-
-
+    uri = URI.parse()self
+    response = Net::HTTP.get_response_body(uri)
+    response.body
   end
-
 end
   
     
