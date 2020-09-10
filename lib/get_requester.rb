6d326fb59876b8213f508ae2f7f14397980c8419
Sara A <calisaraalhindi@gmail.com>
2020-09-10 05:45:30 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,6 +7,9 @@ class GetRequester
   def initialize(url)  
     url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   end
+  
+  def get_response_body
+    
 
 end
   
