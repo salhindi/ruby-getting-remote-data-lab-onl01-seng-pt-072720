6cf0bb94909d23dae714fd6b007aeb7502d869c3
Sara A <calisaraalhindi@gmail.com>
2020-09-10 05:40:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,10 +4,10 @@ require 'json'
 
 class GetRequester
   
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
   def get_response_body
-    uri = URI.parse(url)
+    uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
